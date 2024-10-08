0634452da734cf0ed682c012526673c4ac1108c9
rajsekhar <rajsekhar9655@gmail.com>
2024-10-08 17:51:29 +0530

    updated sec

---
+++ varables.tf
@@ -0,0 +1,34 @@
+variable "project_name" {
+  default = { 
+
+  }
+  
+}
+
+variable "environment" {
+   default = { 
+    
+  }
+ 
+}
+
+variable "sg_name" {
+  default = {
+    
+  }
+}  
+
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
+
+variable "vpc_id" {
+    
+}
+
